5d5e09211f6631b78e54c2c5642d9d5731331283
chaitanya <krishnachaitanya@lyrostech.com>
2024-03-14 19:04:02 +0530

    commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { FaShoppingCart } from "react-icons/fa";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import App, { UserContext } from "../../App";
 import { AppContext } from "../../context/appContext";
-import { TbBrandProducthunt } from "react-icons/tb";
-import { IoIosLogOut } from "react-icons/io";
-import { FaJediOrder } from "react-icons/fa6";
-
+import { FaShoppingCart } from "react-icons/fa";
+import { IoLogOutSharp } from "react-icons/io5";
 export default function Navbar() {
   const { flag, setFlag, user, setUser } = useContext(UserContext);
   const { cartItems,orders } = useContext(AppContext);
@@ -20,11 +17,12 @@ export default function Navbar() {
       <div className="title">Irish Cafe</div>
       <div><h3>Hi {user.name}!</h3></div>
       <div className="links">
-        <Link to={`${PATH}/`}><TbBrandProducthunt /> Products </Link>
-        <Link to={`${PATH}/order`}> <FaJediOrder />Orders({orders.length}) </Link>
-        <Link to={`${PATH}/cart`}><FaShoppingCart /> Cart({total}) </Link>
-        <Link to={`${PATH}/`} onClick={() => setFlag((prev) => 0)}><IoIosLogOut />
-          Logout
+        <Link to={`${PATH}/`}> Products </Link>
+        <Link to={`${PATH}/order`}> Orders({orders.length}) </Link>
+        {total > 0 && ( <Link to={`${PATH}/cart`}><FaShoppingCart /> Cart({total}) </Link>
+      )}
+        <Link to={`${PATH}/`} onClick={() => setFlag((prev) => 0)}>
+        <IoLogOutSharp />Logout
         </Link>
       </div>
     </div>
